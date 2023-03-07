2fa9cdba28e91172c50b155243640135dba96869
ShreyCitrusbug <shrey.citrusbug@gmail.com>
2023-03-07 17:18:45 +0530

    Javascript Learning and Webpage Design

--- Readme.md
+++ Readme.md
@@ -1,3 +1,20 @@
 #learning html
 Date 03/03/2023
 Learn Javascript from w3school.com
+
+Date 07/03/2023
+
+JavaScript Learning
+
+ - Tasks
+     - JavaScript HTML -CSS DOM 
+     - JavaScript Events
+     - JavaScript Event Listener
+     - JavaScript Nodes and Navigation
+     - JavaScript DOM Collections and Node Lists
+     - JavaScript Loops and Conditions
+     - JavaScript Arrow Functions
+     - Atlassian website design - Completed
+
+ - Next-Day Tasks
+     - jQuery Learning
--- static/js/text.js
+++ static/js/text.js
@@ -6,7 +6,13 @@ document.getElementById('with-team').innerHTML = 'Hello World!!';
 document.getElementById('with-team').innerHTML= ele;
 const b=document.getElementsByName('try');
 let arr=document.getElementsByTagName("*");
-let tag=""
+const nod = document.createElement('p');
+const n = document.createTextNode('This is Node Element!');
+nod.appendChild(n);
+const ele1 = document.getElementById("test");
+ele1.appendChild(nod);
+
+let tag="";
 for (let i=0; i<arr.length;i++)
 {
   tag += arr[i].tagName+"<br/>";
@@ -34,4 +40,23 @@ function myFun(p1,p2)
 const person={name:"Sachin",age:42,lastName:"Tendulkar",runs:18000};
 let x=myFun(4,5);
 let text="The sum of two numbers:" + x;
-document.getElementById("with-team").innerHTML = "God of Cricket is "+ person.name + person.lastName + " He has " + person.runs+ " International Runs."; 
+document.getElementById("with-team").innerHTML = "God of Cricket is "+ person.name + person.lastName + " He has " + person.runs+ " International Runs.";
+function over(con)
+{
+  con.innerHTML = "Join!!!"
+}
+function out(ref)
+{
+  ref.innerHTML = "Out!!!"
+}
+function up(bac)
+{
+  bac.innerHTML = "Management!"
+  bac.style.backgroundColor = "white"
+}
+function down(jl)
+{
+  jl.innerHTML = "Work Manage!"
+  jl.style.color= "#0052cc"
+  // jl.style.color = 
+}
